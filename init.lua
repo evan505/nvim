dfcbc5209fee9b9a08e501170e3b1b4cb4a51ddf
zhangyifan <zhangyifan@bilibili.com>
2023-04-12 11:01:48 +0800

    feat(config):auto swich input

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
 -- 基础配置
 require("basic")
--- Packer插件管理
-require("plugins")
 -- 快捷键映射
 require("keybindings")
+-- Packer插件管理
+require("plugins")
 -- 主题设置
 require("colorscheme")
+-- 自动命令
+require("autocmds")
 -- 插件配置
 require("plugin-config.nvim-tree")
 require("plugin-config.bufferline")
